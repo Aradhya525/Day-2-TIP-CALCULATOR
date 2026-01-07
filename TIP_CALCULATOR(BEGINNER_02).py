30de223fee9c9fb32157b3de339f9f2f93b345ab
Aradhya525 <an8038@srmist.edu.in>
2026-01-08 00:58:29 +0530

    Add files via upload

---
+++ TIP_CALCULATOR(BEGINNER_02).py
@@ -0,0 +1,7 @@
+print("Welcome to TIP Calculator(Bkl paise sb ko bhrne hai bkchodi nhi)")
+bill_amt = float(input("Enter the bill amount $"))
+tip = int(input("What percentage tip would you like to give? 10% 15% 25%: %"))
+people_number = int(input("Enter number of Male paying the bill: "))
+total_bill = bill_amt + bill_amt * (tip/100)  
+bill_split = total_bill/people_number
+print(f"Each person should pay {bill_split}")
